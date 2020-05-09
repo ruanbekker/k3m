e8bf55ff8cbb68626ca8583691b4d4af7ad4cba0
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-05-10 01:23:33 +0200

    check if multipass is installed

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+# is multipass installed?
+which multipass  &> /dev/null && MULTIPASS_EXIT_CODE=${?} || MULTIPASS_EXIT_CODE=${?}
+if [ "${MULTIPASS_EXIT_CODE}" == 1 ]
+then
+  echo "Multipass is not installed:"
+  echo "https://multipass.run/docs"
+  exit 1
+fi
+

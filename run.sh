8fc43c1a822cda29f0e2c30976c8a4a8b6d0b24c
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-05-10 01:51:53 +0200

    check if provided ssh key exists

--- run.sh
+++ run.sh
@@ -9,3 +9,23 @@ then
   exit 1
 fi
 
+# global environment variables
+K3M_PATH=~/.k3m
+K3M_INSTANCE_IMAGE="bionic"
+K3M_INSTANCE_NAME="k3s"
+K3M_CLOUD_INIT="cloud-init-k3s.yml"
+K3M_SSH_PUBLIC_KEY=~/.ssh/id_rsa.pub
+K3M_SSH_PUBLIC_KEY_CONTENT="$(cat ${K3M_SSH_PUBLIC_KEY})"
+
+# create k3m home directory
+mkdir -p ${K3M_PATH}
+
+# check if specified public key exists
+if ! [ -f "${K3M_SSH_PUBLIC_KEY}" ] 
+  then 
+    # create key
+    echo "Specified key does not exist, creating ssh key ~/.k3m/ssh_key"
+    ssh-keygen -b 2048 -f ${K3M_PATH}/ssh_key -t rsa -C "k3m" -q -N ""
+    K3M_SSH_PUBLIC_KEY=${K3M_PATH}/ssh_key.pub
+    K3M_SSH_PUBLIC_KEY_CONTENT="$(cat ${K3M_SSH_PUBLIC_KEY})"
+fi
